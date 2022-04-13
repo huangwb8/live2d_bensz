7d9817986084ca1761a60fc489274c78abf2c4a5
huangwb8 <654751191@qq.com>
2022-04-13 12:58:44 +0800

    test
    test

--- front/autoload.js
+++ front/autoload.js
@@ -1,5 +1,5 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/huangwb8/live2d_bensz@latest/front/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/huangwb8/live2d_bensz@latest/hijiki/front/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
@@ -33,7 +33,7 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			apiPath: "https://cdn.jsdelivr.net/gh/huangwb8/live2d_bensz@latest/back/",
+			apiPath: "https://cdn.jsdelivr.net/gh/huangwb8/live2d_bensz@latest/hijiki/back/",
 			//cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
 		});
 	});
